7b5c8ce4f485d2c6f398bfb22bf1f8904820c908
Bensolo Doe <charlesbdoe@outlook.com>
2023-02-01 09:22:36 -0500

    Initial Commit

---
+++ The Immigreats/The_ImmigreatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_ImmigreatsApp.swift
+//  The Immigreats
+//
+//  Created by Charles Doe on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct The_ImmigreatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
